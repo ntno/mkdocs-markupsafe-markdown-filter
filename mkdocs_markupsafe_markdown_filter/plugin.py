8e40089a309f539e58f9ea269095859553645c0b
ntno <ntno@users.noreply.github.com>
2025-04-07 21:36:41 -0400

    signature update

--- mkdocs_markupsafe_markdown_filter/plugin.py
+++ mkdocs_markupsafe_markdown_filter/plugin.py
@@ -5,14 +5,16 @@ or by marking the text as safe.
 
 See markupsafe for details: https://github.com/pallets/markupsafe
 """
-
-from mkdocs.plugins import BasePlugin
 import markdown
 import markupsafe
+import jinja2.environment
+from mkdocs.plugins import BasePlugin
+from mkdocs.structure.files import Files
+from mkdocs.config.defaults import MkDocsConfig
 
 class MarkupSafeMarkdownFilterPlugin(BasePlugin):
     """
-    Registers the markupsafe 'Markup' function as a filter with the name 'markdown'
+    Registers the markupsafe 'Markup' function as a Jinja filter with the name 'markdown'
     """
 
     config_scheme = (
@@ -33,9 +35,21 @@ class MarkupSafeMarkdownFilterPlugin(BasePlugin):
         )
         return markupsafe.Markup(md.convert(text))
 
-    def on_env(self, env, config):
+    def on_env(
+        self, env: jinja2.Environment, /, *, config: MkDocsConfig, files: Files
+    ) -> jinja2.Environment | None:
         """
-        A Global MkDocs event which will trigger once per build
+        The `env` event is called after the Jinja template environment is created
+        and can be used to alter the
+        [Jinja environment](https://jinja.palletsprojects.com/en/latest/api/#jinja2.Environment).
+
+        Args:
+            env: global Jinja environment
+            config: global configuration object
+            files: global files collection
+
+        Returns:
+            global Jinja Environment
         """
         self.config = config
         env.filters['markdown'] = self.md_filter
