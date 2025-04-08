1c4bee54af8d49158bc42fda429999e77832bf8f
ntno <ntno@users.noreply.github.com>
2025-04-08 19:43:40 -0400

    Add author email and security policy (#6)
    * can't leave email blank otherwise invalid link shows up in pypi (link title = natan, link href = byrne's email address)
    * security policy
    * patch version

--- mkdocs_markupsafe_markdown_filter/__version__.py
+++ mkdocs_markupsafe_markdown_filter/__version__.py
@@ -4,4 +4,4 @@ Provides the current version information for the package.
 The version is stored as a string in the 'VERSION' variable, following semantic versioning.
 """
 
-VERSION="0.1.0"
+VERSION="0.1.1"
